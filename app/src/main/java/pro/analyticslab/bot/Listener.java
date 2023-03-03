39f23c4e86338592be0f46494271283d1ca7b6c9
zhaba <zhaba.v.kedah77@gmail.com>
2023-03-03 23:41:29 +0500

    Activity set

--- app/src/main/java/pro/analyticslab/bot/Listener.java
+++ app/src/main/java/pro/analyticslab/bot/Listener.java
@@ -1,10 +1,13 @@
 package pro.analyticslab.bot;
 
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
 
 public class Listener extends ListenerAdapter {
     private final Logger logger = LoggerFactory.getLogger("Listener");
@@ -12,5 +15,9 @@ public class Listener extends ListenerAdapter {
     @Override
     public void onReady(@NotNull ReadyEvent event) {
         logger.info("Connected to " + event.getJDA().getSelfUser().getAsTag());
+
+        // activity
+        Objects.requireNonNull(event.getJDA().getShardManager()).setActivity(Activity.playing("Terraria"));
+        logger.info("Activity initialized");
     }
 }
